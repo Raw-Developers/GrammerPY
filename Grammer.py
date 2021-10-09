e9beac17c04d9ac90d2999dde6554ff6d0458842
delta114514 <gey3933@gmail.com>
2021-10-09 15:37:23 +0900

    remove print() and made to print \n

--- Grammer.py
+++ Grammer.py
@@ -10,6 +10,6 @@ with open('transcript1.txt', 'r') as f:
     for line in f:
         matches.extend(tool.check(line))
 
-print("No. of mistakes found in document is", len(matches))
+print("No. of mistakes found in document is", len(matches), "\n")
 for mistake in matches:
-    print(mistake)
+    print(mistake, "\n")
